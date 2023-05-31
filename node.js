99129f1273bd31428833578546732ef6dd0e7345
Le Khac Phuong <phuonglk@nal.vn>
2023-05-31 15:12:20 +0700

    hotfix: use hardcoded proxy

--- node.js
+++ node.js
@@ -1,15 +1,18 @@
 module.exports = function (RED) {
   "use strict";
   var translate = require("@vitalets/google-translate-api");
+  var {HttpProxyAgent} = require('http-proxy-agent');
 
   function GoogleTranslateNode(config) {
     RED.nodes.createNode(this, config);
     var node = this;
+    const agent = new HttpProxyAgent('http://212.107.31.118:80');
 
     this.on("input", function (msg) {
       const conf = {
         from: config.from === "prog" ? msg.payload.from : config.from,
         to: config.from === "prog" ? msg.payload.to : config.to,
+        fetchOptions: { agent }
       };
 
       const phrase = msg.payload?.phrase ?? msg.payload;
